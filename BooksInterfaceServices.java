68e46557979496c6e7b41b826e5b5eab0b60d4e0
revanrn <59168101+revanrn@users.noreply.github.com>
2020-01-03 12:42:51 +0530

    Delete BooksInterfaceServices.java

--- BooksInterfaceServices.java
+++
@@ -1,7 +0,0 @@
-package com.deloitte.library.services;
-
-import com.deloitte.library.model.Books;
-
-public interface BooksInterfaceServices {
-	public void createBooks(String name1, float price);
-}
